c8b605a4cb2eb63f402c48932ca240f93fe3b397
Nikita Osadchyi <nikitaosadchyi7@gmail.com>
2022-04-10 16:07:05 +0300

    fetch images & indec.js

--- src/fetchImages.js
+++ src/fetchImages.js
@@ -7,14 +7,14 @@ export default class ImagesApiService {
     constructor() {
         this.searchQuery = '';
         this.page = 1;
-    };
+    }
 
     async fetchImages() {
-        const url = `${BASE_URL}/?key=${API_KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=${this.page}`;
+        const url = `${BASE_URL}/?key=${API_KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.page}`;
         const response = await axios.get(url);
         this.page += 1;
         return response.data;
-    };
+    }
 
     resetPage() {
         this.page = 1;
@@ -22,9 +22,9 @@ export default class ImagesApiService {
 
     get query() {
         return this.searchQuery;
-    };
+    }
 
     set query(newQuery) {
         this.searchQuery = newQuery;
-    };
-};
+    }
+}
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import "simplelightbox/dist/simple-lightbox.min.css";
 import SimpleLightbox from "simplelightbox";
 import ImagesApiService from './fetchImages';
+import simpleLightbox from 'simplelightbox';
 
 const refs = {
     searchForm: document.querySelector(".search-form"),
@@ -22,5 +23,65 @@ function onSearch(event) {
     totalImages = 0;
 };
 
-function renserImages
+async function fetchGallery() {
+    try {
+        const newImages = await imagesApiService.fetchImages();
+        return renderImages(newImages);
+    }
+    catch (error) {
+        onFetchError();
+    };
+};
+
+function renderImages() {
+    if (images.hits === 0) {
+        refs.loadMoreBtn.classList.add('is-hidden');
+        Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+    } else {
+        const markup = images.hits.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => `
+        <div class ="photo-card" href =""${largeImageURL}>
+            <img class = "gallery__image" src = "${webformatURL}" alt ="${tags}" loading = "lazy" />
+            <div class ="info">
+                <p class="info-item">
+                    <b>Likes</b><br>${likes}
+                </p>
+                <p class="info-item">
+                    <b>Views</b><br>${views}
+                </p>
+                <p class="info-item">
+                    <b>Comments</b><br>${comments}
+                </p>
+                <p class="info-item">
+                    <b>Downloads</b><br>${downloads}
+                </p>
+            </div>
+        </div>
+        `).join('');
+
+        refs.gallery.insertAdjacentHTML('beforeend', markup);
+        refs.loadMoreBtn.classList.remove('is-hidden');
+
+        totalImages += 40;
+        if (totalImages === 40) {
+            Notify.success(`We found ${totalHits} images for you.`)
+        };
+        if (images.totalHits <= totalImages) {
+            Notify.failure("We're sorry, but you've reached the end of search results.")
+            refs.loadMoreBtn.classList.add('is-hidden')
+        };
+    };
+    const lightBox = new SimpleLightbox('.gallery div', { captionDelay: 250 });
+    if (totalImages > 40) {
+        lightbox.refresh();
+        const { height: cardHeight } = document
+            .querySelector(".gallery")
+            .firstElementChild.getBoundingClientRect();
+        window.scrollBy({ top: cardHeight, behavior: "smooth", });
+    };
+};
+
+function onFetchError() {
+    Notify.failure('Oops, there is no country with that name');
+    console.log(error);
+}
 
