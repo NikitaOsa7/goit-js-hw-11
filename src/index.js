07f45e96c477636d06a44cfd26c2b40bcd3b2110
Nikita Osadchyi <nikitaosadchyi7@gmail.com>
2022-04-10 15:17:03 +0300

    imports

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 import './sass/main.scss';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from "simplelightbox";
+import ImagesApiService from './fetchImages';
+
+const refs = {
+    searchForm: document.querySelector(".search-form"),
+    loadMoreBtn: document.querySelector(".load-more"),
+    gallery: document.querySelector(".gallery"),
+};
+
+const imagesApiService = new ImagesApiService();
+let totalImages = 0;
+
+function onSearch(event) {
+    event.preventDefault();
+    refs.gallery.innerHTML = '';
+    refs.loadMoreBtn.classList.add('is-hidden')
+};
+
